8f53146bcfc20e1e4d2629e5fd5ff86f8b54925e
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2023-05-18 16:16:02 +0200

    fully functioning portfolio project, i hope i will mantain(update) this project in the future.

--- frontend_sanity-myportfolio/src/components/SocialMedia.jsx
+++ frontend_sanity-myportfolio/src/components/SocialMedia.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { FaFacebookF, FaGithub } from 'react-icons/fa';
 
 const SocialMedia = () => (
-  <div className="app__social">
+  <div className='app__social'>
+    <div>
+      <FaGithub />
+    </div>
     <div>
       <BsTwitter />
     </div>
